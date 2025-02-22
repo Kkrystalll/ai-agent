b94ad6d3c4f70b4d9e60ddf3e7f4339687c97bb2
krystal <krystal93002232@gmail.com>
2025-02-22 10:57:11 +0800

    feature: import open AI & sent message

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from config import OPEN_API_KEY
+from openai import OpenAI
+client = OpenAI(api_key=OPEN_API_KEY)
 
-print(OPEN_API_KEY)
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "user", "content": "早安"},
+    ]
+)
+
+print(response.choices[0].message)
